227291abf89576bf362663d8bbd6b08ba5ea6bb9
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2015-05-31 19:38:47 +0200

    Removes DCIntrospect submodule. Refs #37.

--- src/App_delegate.m
+++ src/App_delegate.m
@@ -8,12 +8,6 @@
 
 #import "EHReachability.h"
 
-#ifdef USE_DCINTROSPECT
-#ifdef TARGET_IPHONE_SIMULATOR
-#import "DCIntrospect.h"
-#endif
-#endif
-
 /** \mainpage Record my position
  *
  * \section meta Meta
@@ -93,11 +87,6 @@ static void _set_globals(void);
 
 	[window_ makeKeyAndVisible];
 
-#ifdef USE_DCINTROSPECT
-#if TARGET_IPHONE_SIMULATOR
-	[[DCIntrospect sharedIntrospector] start];
-#endif
-#endif
 	return YES;
 }
 
